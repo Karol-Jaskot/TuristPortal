8f68ececbd1832bbdeee0f9651e1ed4616f26742
Karol <karol.jaskot@wp.pl>
2021-10-06 19:27:22 +0200

    Refactoring of entity and simple data generating

--- src/main/java/pl/jaskot/turistportal/backend/entity/Country.java
+++ src/main/java/pl/jaskot/turistportal/backend/entity/Country.java
@@ -1,21 +1,20 @@
 package pl.jaskot.turistportal.backend.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Country {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String name;
-    private String imgFlag;
-    private String imgPhoto;
-    private String textOffer;
+    private String name, imgFlag, imgPhoto, textOffer;
     private int price;
 
     public Country() {}
@@ -26,53 +25,5 @@ public class Country {
         this.imgPhoto = imgPhoto;
         this.textOffer = textOffer;
         this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTextOffer() {
-        return textOffer;
-    }
-
-    public void setTextOffer(String textOffer) {
-        this.textOffer = textOffer;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public String getImgFlag() {
-        return imgFlag;
-    }
-
-    public void setImgFlag(String imgFlag) {
-        this.imgFlag = imgFlag;
-    }
-
-    public String getImgPhoto() {
-        return imgPhoto;
-    }
-
-    public void setImgPhoto(String imgPhoto) {
-        this.imgPhoto = imgPhoto;
     }
 }
--- src/main/java/pl/jaskot/turistportal/backend/generators/QuestionGenerator.java
+++ src/main/java/pl/jaskot/turistportal/backend/generators/QuestionGenerator.java
@@ -13,39 +13,25 @@ public class QuestionGenerator {
 
         if (questionRepo.count() == 0 ) {
             logger.info("Start generate questions for application");
-            Question q1 = new Question(1, "Czy lubisz ciepłe kraje?");
-            questionRepo.save(q1);
 
-            Question q2 = new Question(2, "Czy lubisz latynoskie klimaty?");
-            Question q3 = new Question(3, "Czy lubisz skandynawskie klimaty?");
-            questionRepo.save(q2);
-            questionRepo.save(q3);
+            questionRepo.save(new Question(1, "Czy lubisz ciepłe kraje?"));
 
-            Question q4 = new Question(4, "Czy lubisz góry?");
-            Question q5 = new Question(5, "Czy lubisz zwiedzać zabytkowe miejsca?");
-            Question q6 = new Question(6, "Czy lubisz góry?");
-            Question q7 = new Question(7, "Czy lubisz morze?");
-            questionRepo.save(q4);
-            questionRepo.save(q5);
-            questionRepo.save(q6);
-            questionRepo.save(q7);
+            questionRepo.save(new Question(2, "Czy lubisz latynoskie klimaty?"));
+            questionRepo.save(new Question(3, "Czy lubisz skandynawskie klimaty?"));
 
-            Question q8 = new Question(8, "Czy lubisz odpoczywać na plaży?");
-            Question q9 = new Question(9, "Czy chciałbyś zobaczyć lasy tropikalne?");
-            Question q10 = new Question(10, "Czy lubisz tereny zielone?");
-            Question q11 = new Question(11, "Czy lubisz kulturę arabską?");
-            Question q12 = new Question(12, "Czy lubisz fiordy?");
-            Question q13 = new Question(13, "Czy lubisz leśne spacery?");
-            Question q14 = new Question(14, "Czy lubisz góry?");
-            Question q15 = new Question(15, "Czy jesteś osobą tolerancyjną?");
-            questionRepo.save(q8);
-            questionRepo.save(q9);
-            questionRepo.save(q10);
-            questionRepo.save(q11);
-            questionRepo.save(q12);
-            questionRepo.save(q13);
-            questionRepo.save(q14);
-            questionRepo.save(q15);
+            questionRepo.save(new Question(4, "Czy lubisz góry?"));
+            questionRepo.save(new Question(5, "Czy lubisz zwiedzać zabytkowe miejsca?"));
+            questionRepo.save(new Question(6, "Czy lubisz góry?"));
+            questionRepo.save(new Question(7, "Czy lubisz morze?"));
+
+            questionRepo.save(new Question(8, "Czy lubisz odpoczywać na plaży?"));
+            questionRepo.save(new Question(9, "Czy chciałbyś zobaczyć lasy tropikalne?"));
+            questionRepo.save(new Question(10, "Czy lubisz tereny zielone?"));
+            questionRepo.save(new Question(11, "Czy lubisz kulturę arabską?"));
+            questionRepo.save(new Question(12, "Czy lubisz fiordy?"));
+            questionRepo.save(new Question(13, "Czy lubisz leśne spacery?"));
+            questionRepo.save(new Question(14, "Czy lubisz góry?"));
+            questionRepo.save(new Question(15, "Czy jesteś osobą tolerancyjną?"));
 
             logger.info("Finish generate questions for application");
         }else {
