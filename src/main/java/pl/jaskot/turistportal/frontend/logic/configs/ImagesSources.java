5a736f1ad92115a49756bd7ea06df9ec16a9e1be
Karol <karol.jaskot@wp.pl>
2021-10-06 19:38:13 +0200

    Better logo images

--- src/main/java/pl/jaskot/turistportal/frontend/logic/configs/ImagesSources.java
+++ src/main/java/pl/jaskot/turistportal/frontend/logic/configs/ImagesSources.java
@@ -1,9 +1,8 @@
 package pl.jaskot.turistportal.frontend.logic.configs;
 
 public class ImagesSources {
-
-    public static final String springLogo = "https://camo.githubusercontent.com/12136cf9daa20a57168a9bdee376f2e83e13c5b1/68747470733a2f2f7069636f636c692e696e666f2f696d616765732f737072696e672d626f6f742e706e67";
-    public static final String hibernateLogo = "https://miro.medium.com/max/1200/1*GI9m2zoDYsNNU59q20nMsg.jpeg";
+    public static final String springLogo = "https://miro.medium.com/max/700/1*-uckV8DOh3l0bCvqZ73zYg.png";
+    public static final String hibernateLogo = "https://upload.wikimedia.org/wikipedia/commons/2/22/Hibernate_logo_a.png";
     public static final String vaadinLogo = "https://vaadin.com/images/trademark/PNG/VaadinLogo_RGB_1000x310.png";
     public static final String menuLogo = "https://www.goforworld.com/wp-content/uploads/2017/01/travel_map.jpg";
 }
